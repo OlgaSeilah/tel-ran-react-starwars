8391fc6ad53125e8b6792228be822d4f8f516e8a
seilah <princenoru@gmail.com>
2025-07-03 16:44:38 +0200

    hw-27-react-starwars: added DreamTeamItem component

--- src/components/DreamTeamItem.jsx
+++ src/components/DreamTeamItem.jsx
@@ -1,14 +1,15 @@
 const DreamTeamItem = ({pictureName}) => {
     let address = `src/assets/${pictureName}.jpg`;
 
-    let pictures = (
-            <>
-                <img className="col-sm-4 p-1" src={address} alt="Friend"/>
-            </>
-        )
-    ;
+    if (pictureName === "friend7") {
+        return <img className="bottomLeft col-sm-4 p-1" src={address} alt="Friend"/>
+    } else if (pictureName === "friend9") {
+        return <img className="bottomRight col-sm-4 p-1" src={address} alt="Friend"/>
+    }
 
-    return pictures;
+    return (
+        <img className="col-sm-4 p-1" src={address} alt="Friend"/>
+    );
 }
 
 export default DreamTeamItem;
