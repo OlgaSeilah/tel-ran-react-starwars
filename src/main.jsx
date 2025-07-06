de5ddf1d499e3ca22279d352af38ee1246deb2e0
seilah <princenoru@gmail.com>
2025-07-06 20:01:17 +0200

    update OpeningCrawl.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,8 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+    // позволяет найти ошибки архитектуры (устаревшие проблемы)
+  // <StrictMode>
     <App />
-  </StrictMode>,
+  // </StrictMode>,
 )
